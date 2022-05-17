0d2f4af0ce922cf67c03da81f797ffcd719486f6
whatserface <perevernusb@gmail.com>
2022-05-17 17:30:03 +0200

    Сделали компоненты абзаца и тэга (дива)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Htag, Button } from '../components';
+import { Htag, Button, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +7,13 @@ export default function Home(): JSX.Element {
       <Htag tag="h1">Текст</Htag>
       <Button appearance='primary' arrow='right'>Кнопка</Button>
       <Button appearance='ghost' arrow='right'>Кнопка</Button>
+      <P size='s'>Малой</P>
+      <P size='m'>Средний</P>
+      <P size='l'>Большой</P>
+      <Tag size='s' color='ghost'>Ghost</Tag>
+      <Tag size='m' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Green</Tag>
+      <Tag color='primary'>Primary</Tag>
     </div>
   );
 }
