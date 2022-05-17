ce542f1f318c2813cb4d2f4feff6795f31884ab4
whatserface <perevernusb@gmail.com>
2022-05-17 18:08:47 +0200

    Осмотрели основы того как работать с хуками useEffect и useState

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Htag, Button, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+    console.log('Counter: ' + counter);
+    return function cleanup() {
+      console.log('Unmount');
+    }
+  });
+
   return (
     <div>
-      <Htag tag="h1">Текст</Htag>
-      <Button appearance='primary' arrow='right'>Кнопка</Button>
-      <Button appearance='ghost' arrow='right'>Кнопка</Button>
+      <Htag tag="h1">{counter}</Htag>
+      <Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
+      <Button appearance='ghost' arrow='right' onClick={() => setCounter(x => x - 1)}>Кнопка</Button>
       <P size='s'>Малой</P>
       <P size='m'>Средний</P>
       <P size='l'>Большой</P>
