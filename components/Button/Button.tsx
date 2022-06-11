0bb08da0a49afbc40004547b8667b99b8b4a7674
whatserface <perevernusb@gmail.com>
2022-06-11 19:36:52 +0200

    Построили сайт сеткой (grid)

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,5 @@
-import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
+import { ButtonProps } from './Button.props';
 import ArrowIcon from './arrow.svg';
 import cn from 'classnames';
 
@@ -8,7 +8,7 @@ export const Button = ({ appearance, arrow = 'none', children, className, ...pro
 		<button
 			className={cn(styles.button, className, {
 				[styles.primary]: appearance == 'primary',
-				[styles.ghost]: appearance == 'ghost'
+				[styles.ghost]: appearance == 'ghost',
 			})}
 			{...props}
 		>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { Htag, Button, P, Tag, Rating } from '../components';
+import React, { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
 
 function Home(): JSX.Element {
-  const [rating, setRating] = useState<number>(4);
+	const [rating, setRating] = useState<number>(4);
 
-  return (
-    <>
-      <Htag tag="h1">Заголовок</Htag>
-      <Button appearance='primary' arrow='right'>Кнопка</Button>
-      <Button appearance='ghost' arrow='right'>Кнопка</Button>
-      <P size='s'>Малой</P>
-      <P size='m'>Средний</P>
-      <P size='l'>Большой</P>
-      <Tag size='s' color='ghost'>Ghost</Tag>
-      <Tag size='m' color='red'>Red</Tag>
-      <Tag size='s' color='green'>Green</Tag>
-      <Tag color='primary'>Primary</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}></Rating>
-    </>
-  );
+	return (
+		<>
+			<Htag tag='h1'>Заголовок</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
+			<Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag color='primary'>Green</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
+		</>
+	);
 }
 
 export default withLayout(Home);
