330a1fc470f578cf402791646a52b72e5aaab55d
whatserface <perevernusb@gmail.com>
2022-06-18 20:56:35 +0200

    Завершили роутинг по сайту. Улучшили вёрстку

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -3,21 +3,9 @@ import cn from 'classnames';
 import { useContext } from 'react';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
-import { TopLevelCategory } from '../../interfaces/toppage.interface';
-import { P } from '../../components';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-];
+import { firstLevelMenu } from '../../helpers/helpers';
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
@@ -39,12 +27,11 @@ export const Menu = (): JSX.Element => {
 					<div key={m.route}>
 						<Link href={`/${m.route}`}>
 							<a>
-
 								<div className={cn(styles.firstLevel, {
 									[styles.firstLevelActive]: m.id == firstCategory
 								})}>
 									{m.icon}
-									<span >{m.name}</span>
+									<span>{m.name}</span>
 								</div>
 							</a>
 						</Link>
